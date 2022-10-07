34ceb0a713253e10b83cd167a1046c9220c9a827
Jamie Griffiths <40271350+JamGrif@users.noreply.github.com>
2022-10-07 16:16:25 +0100

    added support for sfx and music

--- include/core/SoundManager.h
+++ include/core/SoundManager.h
@@ -3,42 +3,42 @@
 
 #include "SDL2_mixer/SDL_mixer.h"
 
-enum sound_type
-{
-	SOUND_MUSIC = 0,
-	SOUND_SFX = 1
-};
+//enum sound_type
+//{
+//	SOUND_MUSIC = 0,
+//	SOUND_SFX = 1
+//};
 
 // .ogg for music | .wav for sound effects
 class SoundManager
 {
 public:
-	static SoundManager* Instance()
+	void init();
+	void clean();
+
+	bool loadSound(const std::string& fileName, const std::string& id);
+	bool loadMusic(const std::string& fileName, const std::string& id);
+
+	void playSound(const std::string& id, int loop = false);
+	void playMusic(const std::string& id, int loop = false);
+
+	static SoundManager* Instance() // Instance Get
 	{
-		if (s_pInstance == 0)
-		{
+		if (!s_pInstance)
 			s_pInstance = new SoundManager();
-			return s_pInstance;
-		}
 		return s_pInstance;
 	}
 
-	bool load(std::string fileName, std::string id, sound_type type);
-
-	void playSound(std::string id, int loop);
-	void playMusic(std::string id, int loop);
-
 private:
-	static SoundManager* s_pInstance; // Singleton as there should only be one place the sounds are stored and should be accessible from anywhere in the game
+	static SoundManager* s_pInstance; 
 
-	std::unordered_map<std::string, Mix_Chunk*> m_sfxs;	// Mix_Chunk used for sound effects only
+	std::unordered_map<std::string, Mix_Chunk*> m_sfxs;		// Mix_Chunk used for sound effects only
 	std::unordered_map<std::string, Mix_Music*> m_music;	// Mix_Music used for music tracks only
 
-	SoundManager();
-	~SoundManager();
-
-	//SoundManager(const SoundManager&);
-	//SoundManager& operator= (const SoundManager&);
-
+	SoundManager() {};									// Prevent construction
+	SoundManager(const SoundManager&) {};				// Prevent construction by copying
+	SoundManager& operator=(const SoundManager&) {};	// Prevent assignment
+	~SoundManager() {};									// Prevent unwanted destruction
 };
+typedef SoundManager TheSoundManager;
 
--- src/core/SoundManager.cpp
+++ src/core/SoundManager.cpp
@@ -1,74 +1,80 @@
 #include "pch.h"
 #include "core/SoundManager.h"
 
-
+#define FREQUENCY 22050
+#define FORMAT AUDIO_S16
+#define CHANNELS 2
+#define CHUNK_SIZE 4096
 
 SoundManager* SoundManager::s_pInstance = nullptr;
 
-bool SoundManager::load(std::string fileName, std::string id, sound_type type)
+void SoundManager::init()
 {
-	if (type == SOUND_MUSIC)
-	{
-		Mix_Music* pMusic = Mix_LoadMUS(fileName.c_str());
-
-		if (pMusic == 0)
-		{
-			std::cout << "Could not load music: ERROR - " << Mix_GetError() << std::endl;
-			return false;
-		}
-
-		m_music[id] = pMusic;
-	}
-	else if (type == SOUND_SFX)
-	{
-		Mix_Chunk* pChunk = Mix_LoadWAV(fileName.c_str());
-
-		if (pChunk == 0)
-		{
-			std::cout << "Could not load SFX: ERROR - " << Mix_GetError() << std::endl;
-			return false;
-		}
-
-		m_sfxs[id] = pChunk;
-		return true;
-	}
-
-	return false;
+	// Must be called to setup audio for the game (before sound can be used)
+	Mix_OpenAudio(FREQUENCY, FORMAT, CHANNELS, CHUNK_SIZE);		
 }
 
-void SoundManager::playSound(std::string id, int loop)
+void SoundManager::clean()
+{
+	// Loop through chunks and delete them
+	for (auto const& [key, value] : m_sfxs)
+	{
+		Mix_FreeChunk(value);
+	}
+	m_sfxs.clear();
+
+	// Loop through music and delete them
+	for (const auto& [key, value] : m_music)
+	{
+		Mix_FreeMusic(value);
+	}
+	m_music.clear();
+
+
+	Mix_CloseAudio();
+	Mix_Quit();
+
+	delete s_pInstance;
+}
+
+bool SoundManager::loadSound(const std::string& fileName, const std::string& id)
+{
+	Mix_Chunk* pChunk = Mix_LoadWAV(fileName.c_str());
+
+	if (!pChunk)
+	{
+		std::cout << "Could not load SFX: ERROR - " << Mix_GetError() << std::endl;
+		return false;
+	}
+
+	m_sfxs[id] = pChunk;
+	return true;
+}
+
+bool SoundManager::loadMusic(const std::string& fileName, const std::string& id)
+{
+	
+	Mix_Music* pMusic = Mix_LoadMUS(fileName.c_str());
+
+	if (!pMusic)
+	{
+		std::cout << "Could not load music: ERROR - " << Mix_GetError() << std::endl;
+		return false;
+	}
+
+	m_music[id] = pMusic;
+	return true;
+}
+
+void SoundManager::playMusic(const std::string& id, int loop)
 {
 	Mix_PlayMusic(m_music[id], loop);
 }
 
-void SoundManager::playMusic(std::string id, int loop)
+void SoundManager::playSound(const std::string& id, int loop)
 {
 	// First parameter is for the channel that the sound be played on. -1 tells SDL_mixer to play the sound on any available channel
 	Mix_PlayChannel(-1, m_sfxs[id], loop);
 }
 
 
-SoundManager::SoundManager()
-{
-	// Must be called to setup audio for the game (before sound can be used)
-	Mix_OpenAudio(
-		22050,		// Frequency
-		AUDIO_S16,	// Format
-		2,			// Channels
-		4096);		// Chunksize
-}
-
-//SoundManager::SoundManager(const SoundManager&)
-//{
-//
-//}
-//
-//SoundManager& SoundManager::operator=(const SoundManager&)
-//{
-//
-//}
-
-SoundManager::~SoundManager()
-{
-	Mix_CloseAudio();
-}
