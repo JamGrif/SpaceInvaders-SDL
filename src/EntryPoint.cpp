8280b1be020773c8f7b0cef69ba560189281f46f
Jamie Griffiths <40271350+JamGrif@users.noreply.github.com>
2022-10-18 15:25:50 +0100

    text console only in debug mode

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -33,8 +33,20 @@
 #include "pch.h"
 #include "core/Game.h"
 
+#include "SDL2/SDL.h" // SDL version of main()
+
+// Enable console if debug mode
+#ifdef _DEBUG
+	#include <windows.h>
+	#define startConsole AllocConsole(); freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+#else
+	#define startConsole
+#endif
+
 int main(int argc, char* args[])
 {
+	startConsole;
+
 	if (!TheGame::Instance()->gameInit())
 		return 1;
 	else
